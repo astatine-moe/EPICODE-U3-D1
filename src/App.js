4d15d55c587740b74d5ba1de74ffb63b51aa3672
Morgan <morgan@morganevans.dev>
2022-11-21 11:28:06 +0000

    hw commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Button from "./components/ButtonComponent";
+import Image from "./components/ImageComponent";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Image src="./logo192.png" alt="React logo" />
+            <br></br>
+            <Button name="Button 1" />
+            <Button name="Button 2" />
+            <Button name="Button 3" />
+        </div>
+    );
 }
 
 export default App;
